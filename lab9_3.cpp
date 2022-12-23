59816a0fe6cd66a63b06832a186893d48371639c
Lica <Tanapatlica@gmail.com>
2022-12-23 16:04:50 +0700

    lap9.3 completed

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -1,13 +1,43 @@
-/*
-"Enter your age: "
-"Enter your height: "
-"Enter your bounty: "
-"Your character = "
-"Zoro"
-"Sanji"
-"Usopp"
-"Chopper"
-"Franky"
-"Brook"
-"Jinbe"
-*/
+#include<iostream>
+#include<iomanip>
+#include<string>
+using namespace std;
+
+int main(){
+    int age;
+    string character;
+    cout << "Enter your age: ";
+    cin >> age;
+    if (age<=25){
+        int height;
+        cout << "Enter your height: ";
+        cin >> height;
+        if (height < 100){
+            character = "Chopper";
+        }else if (height < 180){
+            character = "Usopp";
+        }else{
+            int bounty;
+            cout << "Enter your bounty: ";
+            cin >> bounty;
+            if (bounty>1100000000){
+                character = "Zoro";
+            }else{
+                character = "Sanji";
+            }
+        }
+    }else if (age<=60){
+        int bounty;
+        cout << "Enter your bounty: ";
+        cin >> bounty;
+        if (bounty>500000000){
+            character = "Jinbe";
+        }else{
+            character = "Franky";
+        }
+
+    }else{
+        character = "Brook";
+    }
+    cout << "Your character = " << character;
+}
